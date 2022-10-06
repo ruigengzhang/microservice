1b27c05b076299f70b76f00272b9dbf0d36f1ad9
zjjjjjy <99585306+zjjjjjy@users.noreply.github.com>
2022-10-06 11:50:13 -0700

    10/6

---
+++ prod/blog/comments/index.js
@@ -0,0 +1,18 @@
+const express = requrire('express');
+const bodyParser = require('body-parser');
+const{ randomBytes} = require('crtpto');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/posts/:id/comments', (req,res) => {
+
+});
+
+app.post('/posts/:id/comments', (req,res) =>{
+
+});
+
+app.listen(4001, () =>{
+    console.log('Listening on 4001');
+});
